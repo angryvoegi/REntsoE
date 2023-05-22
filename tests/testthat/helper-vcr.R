ba8556750fd664daeb5e2e93b33a8f23ae20e380
angryvoegi <95473696+angryvoegi@users.noreply.github.com>
2023-05-22 15:56:45 +0200

    Update helper-vcr.R - Added mock API Key

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -2,4 +2,14 @@ library("vcr") # *Required* as vcr is set up on loading
 invisible(vcr::vcr_configure(
   dir = "../REntsoE/tests/fixtures"
 ))
+if (!nzchar(Sys.getenv("ENTSOE_KEY"))) {
+  if (dir.exists(vcr_dir)) {
+    # Fake API token to fool our package
+    Sys.setenv("ENTSOE_KEY" = "foobar")
+  } else {
+    # If there's no mock files nor API token, impossible to run tests
+    stop("No API key nor cassettes, tests cannot be run.",
+         call. = FALSE)
+  }
+}
 vcr::check_cassette_names()
