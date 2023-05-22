df8049199a0f30f3db5aa5712d22fdcf35d0ccdf
angryvoegi <95473696+angryvoegi@users.noreply.github.com>
2023-05-22 16:02:05 +0200

    Update helper-vcr.R - changed path of vcr dir

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -2,6 +2,7 @@ library("vcr") # *Required* as vcr is set up on loading
 invisible(vcr::vcr_configure(
   dir = "../REntsoE/tests/fixtures"
 ))
+vcr_dir <- vcr::vcr_test_path("../REntsoE/tests/fixtures")
 if (!nzchar(Sys.getenv("ENTSOE_KEY"))) {
   if (dir.exists(vcr_dir)) {
     # Fake API token to fool our package
