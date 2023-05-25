e7496c8c4b6f0130f0994ab9de4c784518fefcbb
angryvoegi <nicco.voegeli@gmail.com>
2023-05-25 11:03:36 +0200

    Updated vcr

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -3,15 +3,4 @@ invisible(vcr::vcr_configure(
   dir = "../fixtures"
 ))
 vcr_dir <- vcr::vcr_test_path("fixtures")
-if (!nzchar(Sys.getenv("ENTSOE_KEY"))) {
-  if (dir.exists(vcr_dir)) {
-    # Fake API token to fool our package
-    Sys.setenv("ENTSOE_KEY" = "foobar")
-  } else {
-    # If there's no mock files nor API token, impossible to run tests
-    stop("No API key nor cassettes, tests cannot be run.",
-         call. = FALSE)
-  }
-}
 vcr::check_cassette_names()
-
